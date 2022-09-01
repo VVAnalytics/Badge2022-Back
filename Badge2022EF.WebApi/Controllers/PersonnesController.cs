0a3a68fe835acdbb283a42d30840a84cc784da00
PhBarat <80717433+PhBarat@users.noreply.github.com>
2022-09-01 16:39:54 +0200

    Controler Personnes

--- Badge2022EF.WebApi/Controllers/PersonnesController.cs
+++ Badge2022EF.WebApi/Controllers/PersonnesController.cs
@@ -47,14 +47,9 @@ namespace Badge2022EF.WebApi.Controllers
         [Authorization("Admin", "Praticien")]
         public IEnumerable<PersonneEntity> GetAll()
         {
-            return _context.Users.Include(x => x.urole).ToList() ;
-        }
+            //return _context.Users.Include(x => x.urole).ToList() ;
+            return _context.Users.ToList();
 
-        [HttpGet]
-        [Authorization("Admin", "Praticien")]
-        public IEnumerable<PersonneEntity> GetAllAs([FromQuery] string connectAs)
-        {
-            return new ObservableCollection<PersonneEntity>(_context.Users.Include(x => x.urole).ToList());
         }
 
         // GET api/<PersonnesController>/5
@@ -76,6 +71,30 @@ namespace Badge2022EF.WebApi.Controllers
         public async Task<IActionResult> Post([FromQuery] J_Personnes newPersonne)
         {
             PersonneEntity user = new();
+            user.unom = newPersonne.unom;
+            user.uprenom = newPersonne.uprenom;
+            user.Email = newPersonne.Email;
+            user.udate = newPersonne.udate;
+            user.urue = newPersonne.urue;
+            user.ucodep = newPersonne.ucodep;
+            user.uville = newPersonne.uville;
+            user.upays = newPersonne.upays;
+            //user.Id = newPersonne.Id ;
+            user.UserName = newPersonne.UserName;
+            user.NormalizedUserName = newPersonne.NormalizedUserName ;
+            user.Email = newPersonne.Email;
+            user.NormalizedEmail = newPersonne.NormalizedEmail;
+            user.EmailConfirmed = newPersonne.EmailConfirmed;
+            user.PasswordHash = newPersonne.PasswordHash;
+            user.SecurityStamp = newPersonne.SecurityStamp;
+            user.ConcurrencyStamp = newPersonne.ConcurrencyStamp; 
+            user.PhoneNumber = newPersonne.PhoneNumber; 
+            user.PhoneNumberConfirmed = newPersonne.PhoneNumberConfirmed; 
+            user.TwoFactorEnabled = newPersonne.TwoFactorEnabled; 
+            user.LockoutEnd = newPersonne.LockoutEnd; 
+            user.LockoutEnabled = newPersonne.LockoutEnabled; 
+            user.AccessFailedCount= newPersonne.AccessFailedCount;
+
             var result = await _signInManager.CheckPasswordSignInAsync(user, newPersonne.PasswordHash, false);
             if (result.Succeeded)
             {
@@ -101,7 +120,32 @@ namespace Badge2022EF.WebApi.Controllers
         [Authorization("Admin", "Praticien")]
         public async Task<IActionResult> Put(int id, [FromBody] J_Personnes majPersonne)
         {
-            PersonneEntity user = await _userManager.FindByIdAsync(id.ToString());
+            PersonneEntity user = new();
+            user = await _userManager.FindByIdAsync(id.ToString());
+            user.unom = majPersonne.unom;
+            user.uprenom = majPersonne.uprenom;
+            user.Email = majPersonne.Email;
+            user.udate = majPersonne.udate;
+            user.urue = majPersonne.urue;
+            user.ucodep = majPersonne.ucodep;
+            user.uville = majPersonne.uville;
+            user.upays = majPersonne.upays;
+            //user.Id = newPersonne.Id ;
+            user.UserName = majPersonne.UserName;
+            user.NormalizedUserName = majPersonne.NormalizedUserName;
+            user.Email = majPersonne.Email;
+            user.NormalizedEmail = majPersonne.NormalizedEmail;
+            user.EmailConfirmed = majPersonne.EmailConfirmed;
+            user.PasswordHash = majPersonne.PasswordHash;
+            user.SecurityStamp = majPersonne.SecurityStamp;
+            user.ConcurrencyStamp = majPersonne.ConcurrencyStamp;
+            user.PhoneNumber = majPersonne.PhoneNumber;
+            user.PhoneNumberConfirmed = majPersonne.PhoneNumberConfirmed;
+            user.TwoFactorEnabled = majPersonne.TwoFactorEnabled;
+            user.LockoutEnd = majPersonne.LockoutEnd;
+            user.LockoutEnabled = majPersonne.LockoutEnabled;
+            user.AccessFailedCount = majPersonne.AccessFailedCount;
+
             var result = await _signInManager.CheckPasswordSignInAsync(user, majPersonne.PasswordHash, false);
             if (result.Succeeded)
             {
@@ -144,10 +188,6 @@ namespace Badge2022EF.WebApi.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(string email, string password)
         {
-            PersonneEntity p1 = await _userManager.FindByNameAsync(email);
-
-            await _userManager.AddToRoleAsync(p1, "Admin");
-
             PersonneEntity p = await _userManager.FindByNameAsync(email);
 
             if (p != null)
@@ -160,8 +200,10 @@ namespace Badge2022EF.WebApi.Controllers
                             Email = email
                         };
 
-                    RoleEntity q = await _roleManager.FindByIdAsync(p.urole.Select(x=> x.Id).ToString());
-                        BigUsers.Role = q.Name;
+                    //RoleEntity q = await _roleManager.FindByIdAsync(p.urole.Select(x=> x.Id).ToString());
+                    var p4 = await _userManager.GetRolesAsync(p);
+
+                    BigUsers.Role = p4[0];
 
                         var token = _jWTManager.Authenticate(BigUsers);
                         if (token == null)
